c2a8bdc41b08dcbf97b245ef0cd6d0fc859618de
Ruben Sarkisyan <rubens24@uw.edu>
2016-04-12 13:39:16 -0700

    finished class01 blog assignment

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -1,20 +1,33 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
+  // DONE: Use the js object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.data('title', this.title);
+  $newArticle.data('category', this.category);
+  $newArticle.data('author', this.author);
+  $newArticle.data('authorUrl', this.authorUrl);
+  $newArticle.data('publishedOn', this.publishedOn);
+  $newArticle.data('body', this.body);
 
-  $newArticle.attr('data-category', this.category);
-
-  // TODO: Use jQuery to fill in the template with properties
+  // DONE: Use jQuery to fill in the template with properties
   // from this particular Article instance. We need to fill in:
   // the author name and url, the article title and body, and the
   // publication date.
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('section.article-body').html(this.body);
 
   // Include the publication date as a 'title' attribute to show on hover:
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
@@ -24,8 +37,8 @@ Article.prototype.toHtml = function() {
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
-
+  // DONE: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass('template');
   return $newArticle;
 }
 
