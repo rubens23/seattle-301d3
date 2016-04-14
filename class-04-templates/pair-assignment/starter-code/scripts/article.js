e47a1e1c87c1b63782e2b60a201ced099970a594
Ruben Sarkisyan <rubens24@uw.edu>
2016-04-14 13:06:13 -0700

    finished class 04 pair assignment

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -10,13 +10,11 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   var $source = $('#article-template').html();
   var template = Handlebars.compile($source);
-  return template(this);
-};
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
@@ -25,15 +23,9 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-  rawData.forEach(function(articleObject) {
-    articles.push(new Article(articleObject));
-  });
-
-  articles.forEach(function(newArticleObject) {
-    $('articles').append(newArticleObject.toHtml());
-
-});
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
