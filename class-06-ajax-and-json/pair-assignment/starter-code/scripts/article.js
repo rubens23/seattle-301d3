b050f4077cda4b49692f592a0d01d5073c47c853
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-18 13:12:45 -0700

    captured the data from the data file in JSON format, stored it in the Article array on the object constructor, and added it to local storage within the hackerIpsum property.

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -48,16 +48,18 @@ Article.fetchAll = function() {
     // When our data is already in localStorage,
     // we can load it by calling the .loadAll() method,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll() method? Be careful
+    Article.loadAll(//DONE: What do we pass in here to the .loadAll() method? Be careful
       // when handling different data types between here and localStorage! stringify, parseInt
       JSON.parse(localStorage.hackerIpsum)
     );
-    articleView.initIndexPage();//(); //TODO: Change this fake method call to the correct
+    articleView.initIndexPage();//(); //DONE: Change this fake method call to the correct
     // one that will render the index page.
   } else {
-    // TODO: When we don't already have our data, we need to:
+    // DONE: When we don't already have our data, we need to:
     // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?), .getJSON
-      
+    Article.loadAll($.getJSON('../data/hackerIpsum'));
+    localStorage.hackerIpsum = Article.all;
+
     // 2. Store the resulting JSON data with the .loadAll method,
 
     // 3. Cache it in localStorage so we can skip the server call next time,
