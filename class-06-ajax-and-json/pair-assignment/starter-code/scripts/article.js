44986c1a9172ddd6c80c0d58a45fafb55366d9a1
vbenavente <vbenavente@hotmail.com>
2016-04-18 13:30:31 -0700

    fixed files extension and parsed our objects

--- class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/starter-code/scripts/article.js
@@ -59,7 +59,7 @@ Article.fetchAll = function() {
     // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?), .getJSON
 
     // 2. Store the resulting JSON data with the .loadAll method,
-    Article.loadAll($.getJSON('../data/hackerIpsum'));
+    Article.loadAll(JSON.parse($.getJSON('data/hackerIpsum.json'));
     localStorage.hackerIpsum = Article.all;
 
     // 3. Cache it in localStorage so we can skip the server call next time,
